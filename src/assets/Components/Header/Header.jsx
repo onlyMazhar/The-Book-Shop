b0d5d71e584a57a9d96e5e48508175e6e80524a0
Mazhar <mi.sabbir420@gmail.com>
2025-10-05 03:52:53 +0600

    added header cart, account, search section

--- src/assets/Components/Header/Header.jsx
+++ src/assets/Components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../../../index.css'
+import './Header.css'
 import { NavLink } from 'react-router';
 
 const Header = () => {
@@ -15,7 +16,16 @@ const Header = () => {
     ]
     return (
         <div className="all-text  shadow-sm">
-            <div className='container mx-auto navbar bg-base-100 '>
+            <div className='hidden md:flex    border-b-1 border-b-[#e0e0e0] text-left md:text-center container mx-auto justify-between py-2  text-[#777777]'>
+                <p className='text-[.75rem]'>© 2025 BookShop. All rights reserved.</p>
+                <div className='space-x-4 text-[.75rem] w-[20%] flex justify-around'>
+                    <a className="link link-hover">Account</a>
+                    <a className="link link-hover">Cart</a>
+                    <a className="link link-hover">Search</a>
+
+                </div>
+            </div>
+            <div className='container mx-auto navbar bg-base-100 px-0'>
                 <div className="navbar-start ">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -37,7 +47,7 @@ const Header = () => {
 
                 </div>
                 <div className="navbar-center hidden lg:flex md:ml-84">
-                    <ul className="menu menu-horizontal px-1 uppercase space-x-16 md:ml-46">
+                    <ul className="menu menu-horizontal px-1  uppercase space-x-16 md:ml-46">
                         {
                             links.map((link, i) => <NavLink key={i} to={link.link}>{link.name}</NavLink>)
                         }
